56e06af58ff1e2bb828f056f0b5681573215ab4a
Ishaan Das-Basak <ishaandasbasak@gmail.com>
2025-05-10 22:45:29 -0400

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className="centered">YU ScarbsSA</div>
+)
